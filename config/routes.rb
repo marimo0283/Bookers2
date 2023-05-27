74cf51bc19a4dc13f94f5abbe52a1ff7a8a8bfe9
marimo0283 <kugelschreiber1002@gmail.com>
2023-05-27 06:12:59 +0000

    「Add]ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :books, only: [:new, :create, :update, :index, :show, :edit, :destroy]
+  resources :books
   resources :users, only: [:show, :index, :edit, :update]
 
   get '/home/about' => 'homes#about', as: "about"
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 
 end
